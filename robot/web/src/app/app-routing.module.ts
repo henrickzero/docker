434205d4113ba363c11bbfd64e692040c91b9fc3
henrickzero <henrickzero@gmail.com>
2025-06-04 12:26:30 -0300

    ajustes nginx

--- robot/web/src/app/app-routing.module.ts
+++ robot/web/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { RoboComponent } from './robo/robo.component';
 
 const routes: Routes = [
+  { path: '', component: RoboComponent },
   { path: 'robo', component: RoboComponent }
 ];
 
